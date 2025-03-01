53f923f504859745eb87f8d5a091d89698a851b8
Hugo Youf <91455667+HugoYOUF@users.noreply.github.com>
2025-03-01 18:30:22 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,32 +6,25 @@ using SkiaSharp;
 using PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin;
 using System.Diagnostics;
 
-// Classe représentant un nœud (membre de l'association)
-
-
 class Program
 {
     static void Main()
     {
-        string cheminFichier = "soc-karate.txt"; // Spécifiez le chemin du fichier contenant la matrice d'adjacence
+        string cheminFichier = "soc-karate.txt";
         Graphe graphe = new Graphe(34);
-        graphe.InstancierGrapheDepuisFichier(cheminFichier);
 
+        /// Instancier le graphe, parcours et vérification du graphe
+        graphe.InstancierGrapheDepuisFichier(cheminFichier);
         Console.WriteLine("Matrice d'adjacence du graphe :");
         graphe.AfficherMatriceAdjacence();
-
         Console.WriteLine("Parcours en largeur :");
         graphe.ParcoursBFS(1);
-
         Console.WriteLine("Parcours en profondeur :");
         graphe.ParcoursDFS(1);
-
         Console.WriteLine("Le graphe est connexe ? " + graphe.EstConnexe());
 
-        // Génération et affichage du graphe
+        /// Génération et affichage de l'image
         Console.WriteLine("Génération et affichage du graphe");
         graphe.DessinerGraphe("graphe.png");
-        
-
     }
 }
