b86d49d42eda91e9fa429f6461b5c06928fcacc8
Hugo Youf <91455667+HugoYOUF@users.noreply.github.com>
2025-03-01 18:21:29 +0100

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -4,18 +4,22 @@ using System.IO;
 using System.Linq;
 using SkiaSharp;
 
-
 namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
 {
+    /// Classe qui représente un lien entre deux noeuds
     internal class Lien
     {
+        /// Propriété qui contient le noeud source
         public Noeud Source { get; }
+        
+        /// Propriété qui contient le noeud destination
         public Noeud Destination { get; }
+
+        /// Constructeur qui initialise un lien entre deux noeuds
         public Lien(Noeud source, Noeud destination)
         {
-            Source = source;
-            Destination = destination;
+            Source = source;       /// Assigne le noeud source
+            Destination = destination;  /// Assigne le noeud destination
         }   
     }
 }
-
