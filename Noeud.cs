d4adf877d73d1e89b3c39f427273d933b6546023
Hugo Youf <91455667+HugoYOUF@users.noreply.github.com>
2025-03-01 15:16:50 +0100

    Update Noeud.cs

--- Noeud.cs
+++ Noeud.cs
@@ -1,45 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SkiaSharp;
+
 
 namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
 {
     internal class Noeud
     {
-        public int id; /// Identifiant unique du noeud
-        public List<Lien> liens;  /// Liste des liens (relations) connectés à ce noeud
+        public int Id { get; set; }
+        public Noeud(int id) => Id = id;
 
-        public Noeud(int id)  /// Constructeur naturel, initialise le noeud avec son identifiant
-        {
-            this.id = id;
-            this.liens = new List<Lien>();
-        }
-
-        public int Id ///Accès consultation (get) et modification (set) de Id
-        {
-            get { return this.id; }
-            set { this.id = value; }
-        }
-
-        public List<Lien> Liens  ///Accès consultation (get) et modification (set) de Liens
-        {
-            get
-            {
-                if (this.liens == null)
-                {
-                    this.liens = new List<Lien>();
-                }
-                return this.liens;
-            }
-            set { this.liens = value; }
-
-        }
-        public override string ToString()
-        {
-            return "Noeud " + Id;
-        }
     }
-
 }
