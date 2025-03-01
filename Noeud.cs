fd07f284a59f593ef07ad0c19f58bf3b4a2558bb
Hugo Youf <91455667+HugoYOUF@users.noreply.github.com>
2025-03-01 18:26:44 +0100

    Update Noeud.cs

--- Noeud.cs
+++ Noeud.cs
@@ -4,13 +4,15 @@ using System.IO;
 using System.Linq;
 using SkiaSharp;
 
-
 namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
 {
+    // Classe représentant un nœud dans un graphe ou une structure de données similaire
     internal class Noeud
     {
+        /// Id unique pour chaque nœud
         public int Id { get; set; }
-        public Noeud(int id) => Id = id;
 
+        /// Constructeur qui initialise l'ID du nœud
+        public Noeud(int id) => Id = id;
     }
 }
