1cae63803292f5548ef1276c65ae3ffbfe8b858f
Hugo Youf <91455667+HugoYOUF@users.noreply.github.com>
2025-03-01 18:24:24 +0100

    Update Graphe.cs

--- Graphe.cs
+++ Graphe.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using SkiaSharp;
 
-
 namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
 {
     internal class Graphe
@@ -16,6 +15,7 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
         private List<Lien> liens;
         private int taille;
 
+        /// Constructeur du graphe
         public Graphe(int taille)
         {
             this.taille = taille;
@@ -25,6 +25,7 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
             liens = new List<Lien>();
         }
 
+        /// Ajoute un noeud
         public void AjouterNoeud(int id)
         {
             if (!listeAdjacence.ContainsKey(id))
@@ -34,6 +35,7 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
             }
         }
 
+        /// Ajoute un lien entre deux noeuds
         public void AjouterLien(int source, int destination)
         {
             if (!listeAdjacence[source].Contains(destination))
@@ -46,6 +48,7 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
             }
         }
 
+        /// Crée le graphe à partir d'un fichier
         public void InstancierGrapheDepuisFichier(string cheminFichier)
         {
             if (!File.Exists(cheminFichier))
@@ -68,6 +71,7 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
             }
         }
 
+        /// Affiche la matrice d'adjacence
         public void AfficherMatriceAdjacence()
         {
             for (int i = 1; i <= taille; i++)
@@ -79,12 +83,15 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
                 Console.WriteLine();
             }
         }
+
+        /// Parcours BFS
         public void ParcoursBFS(int depart)
         {
             Queue<int> queue = new Queue<int>();
             HashSet<int> visite = new HashSet<int>();
             queue.Enqueue(depart);
             visite.Add(depart);
+
             while (queue.Count > 0)
             {
                 int noeud = queue.Dequeue();
@@ -101,18 +108,21 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
             Console.WriteLine();
         }
 
+        /// Parcours DFS
         public void ParcoursDFS(int depart, HashSet<int> visite = null)
         {
             if (visite == null) visite = new HashSet<int>();
             if (visite.Contains(depart)) return;
             Console.Write(depart + " ");
             visite.Add(depart);
+
             foreach (var voisin in listeAdjacence[depart])
             {
                 ParcoursDFS(voisin, visite);
             }
         }
 
+        /// Vérifie si le graphe est connexe
         public bool EstConnexe()
         {
             HashSet<int> visite = new HashSet<int>();
@@ -120,6 +130,7 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
             return visite.Count == noeuds.Count;
         }
 
+        /// Dessine le graphe
         public void DessinerGraphe(string cheminImage)
         {
             int largeur = 500, hauteur = 500;
@@ -143,13 +154,13 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
             Random rnd = new Random();
             Dictionary<int, SKPoint> positions = new Dictionary<int, SKPoint>();
 
-            // Positionner les nœuds aléatoirement
+            // Positionnement aléatoire des noeuds
             foreach (var noeud in noeuds)
             {
                 positions[noeud.Id] = new SKPoint(rnd.Next(50, largeur - 50), rnd.Next(50, hauteur - 50));
             }
 
-            // Dessiner les liens
+            // Dessin des liens
             foreach (var lien in liens)
             {
                 SKPoint p1 = positions[lien.Source.Id];
@@ -157,7 +168,7 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
                 canvas.DrawLine(p1, p2, paint);
             }
 
-            // Dessiner les nœuds
+            // Dessin des noeuds
             using var nodePaint = new SKPaint { Color = SKColors.Blue, IsAntialias = true };
             foreach (var noeud in noeuds)
             {
@@ -173,4 +184,4 @@ namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
         }
 
     }
-}   
+}
