eb1450f6828b42b85395872b80e117a485193173
Hugo Youf <91455667+HugoYOUF@users.noreply.github.com>
2025-03-01 15:17:09 +0100

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -1,39 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SkiaSharp;
+
 
 namespace PSI_hugo_Youf_Terence_Roumilhac_Akihito_Raffin
 {
     internal class Lien
     {
-        private Noeud source;       /// Noeud source de la relation
-        private Noeud destination;  /// Noeud destination de la relation
-
-
-        public Lien(Noeud source, Noeud destination)  /// Constructeur naturel, crée un lien entre deux nœuds
+        public Noeud Source { get; }
+        public Noeud Destination { get; }
+        public Lien(Noeud source, Noeud destination)
         {
-            this.source = source;
-            this.destination = destination;
-        }
-
-        public Noeud Source ///Accès consultation (get) et modification (set) de Source
-        {
-            get { return this.source; }
-            set { this.source = value; }
-        }
-
-        public Noeud Destination   ///Accès consultation (get) et modification (set) de Destination
-        {
-            get { return this.destination; }
-            set { this.destination = value; }
-        }
-
-        public override string ToString()
-        {
-            return "Lien entre" + Source.Id + " et " + Destination.Id;
-        }
+            Source = source;
+            Destination = destination;
+        }   
     }
 }
 
